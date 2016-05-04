2aad555cc04d859999b745f549196c126b7317d6
Rhys <rimmington@gmail.com>
2016-05-04 15:14:48 +1000

    ENHANCE: Allow specification of service user

--- pkgs/mkService.nix
+++ pkgs/mkService.nix
@@ -1,25 +1,40 @@
-{writeScript, buildEnv, writeTextFile, stdenv}:
-{name, script, description ? "", startWithBoot ? true}@attrs:
+{writeScript, buildEnv, writeTextFile, lib, stdenv, busybox, shadow}:
+{name, script, description ? "", startWithBoot ? true, user ? "root", addUser ? false}@attrs:
 
 let
   service = {
-    inherit script description;
+    inherit description;
     wantedBy = if startWithBoot then [ "multi-user.target" ] else [];
+    serviceConfig.ExecStart = "${rootScript}";
   };
-  scriptDrv = writeScript "${name}-now" ''
-    #!${stdenv.shell}
-    ${script}
-  '';
+  rootScript =
+    let
+      userScript = writeScript "${name}-now" ''
+        #!${stdenv.shell} -e
+        ${script}
+      '';
+    in
+      if user == "root"
+        then userScript
+        else writeScript "${name}-setuid" ''
+          #!${stdenv.shell} -e
+          ${lib.optionalString addUser ''
+            if ! ${stdenv.glibc.bin}/bin/getent passwd ${user} > /dev/null; then
+              ${shadow}/bin/useradd --system --user-group ${user}
+            fi
+          ''}
+          exec ${busybox}/bin/busybox chpst -u ${user} ${userScript}
+        '';
   disnixDrv = buildEnv {
     name = "service-${name}";
     paths = [
-      (writeTextFile { name = "${name}-disnix-process-config"; destination = "/etc/process_config"; text = ''container_process=${scriptDrv}''; })
+      (writeTextFile { name = "${name}-disnix-process-config"; destination = "/etc/process_config"; text = ''container_process=${rootScript}''; })
     ];
   };
 in {
   inherit attrs;
   serviceAttr = builtins.listToAttrs [ { inherit name; value = service; } ];
-  script = scriptDrv;
+  script = rootScript;
   disnix = {
     inherit name;
     pkg = disnixDrv;
