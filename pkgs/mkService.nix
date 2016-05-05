a942f67f8692a78ce23a7bb584f0ebc61e670798
Rhys <rimmington@gmail.com>
2016-05-06 09:59:23 +1000

    NEW: mkService: preStartRootScript

--- pkgs/mkService.nix
+++ pkgs/mkService.nix
@@ -1,48 +1,65 @@
 {writeScript, buildEnv, writeTextFile, lib, stdenv, busybox, shadow}:
-{name, script, description ? "", startWithBoot ? true, user ? "root", addUser ? false}@attrs:
+{name, script, preStartRootScript ? "", description ? "", startWithBoot ? true, user ? "root", addUser ? false}@attrs:
 
 let
   service = {
     inherit description;
     wantedBy = if startWithBoot then [ "multi-user.target" ] else [];
-    serviceConfig.ExecStart = "${rootScript}";
+    serviceConfig = {
+      ExecStart = execStart;
+    } // lib.optionalAttrs (execStartPre != "") { ExecStartPre = execStartPre; };
   };
-  rootScript =
+  execStartPre = if preStartRootScript != "" || addUser
+    then writeScript "${name}-prestart" ''
+      #!${stdenv.shell} -e
+      ${lib.optionalString addUser ''
+        if ! ${stdenv.glibc.bin}/bin/getent passwd ${user} > /dev/null; then
+          ${shadow}/bin/useradd --system --user-group ${user}
+        fi
+      ''}
+      ${preStartRootScript}
+    ''
+    else "";
+  execStart =
     let
-      userScript = writeScript "${name}-now" ''
+      userScript = writeScript "${name}-start" ''
         #!${stdenv.shell} -e
         ${script}
       '';
     in
       if user == "root"
-        then userScript
-        else writeScript "${name}-setuid" ''
-          #!${stdenv.shell} -e
-          ${lib.optionalString addUser ''
-            if ! ${stdenv.glibc.bin}/bin/getent passwd ${user} > /dev/null; then
-              ${shadow}/bin/useradd --system --user-group ${user}
-            fi
-          ''}
-          exec ${busybox}/bin/busybox chpst -u ${user} ${userScript}
-        '';
+        then "${userScript}"
+        else "${busybox}/bin/busybox chpst -u ${user} ${userScript}";
   disnixDrv = buildEnv {
     name = "service-${name}";
     paths = [
-      (writeTextFile { name = "${name}-disnix-process-config"; destination = "/etc/process_config"; text = ''container_process=${rootScript}''; })
+      (writeTextFile { name = "${name}-disnix-process-config"; destination = "/etc/process_config"; text = ''container_process=${execStart}''; })
       (writeTextFile { name = "${name}-disnix-systemd-config"; destination = "/etc/systemd-config"; text = ''
+
         [Unit]
         Description=${description}
+      '' + lib.optionalString (execStartPre != "") ''
+
+        [Service]
+        ExecStartPre=${execStartPre}
       ''; })
     ];
   };
 in {
   inherit attrs;
+
   serviceAttr = builtins.listToAttrs [ { inherit name; value = service; } ];
-  script = rootScript;
+
   disnix = {
     inherit name;
     pkg = disnixDrv;
     dependsOn = {};
     type = "process";
   };
+
+  script = writeScript "${name}-now" ''
+    #!${stdenv.shell} -e
+    ${execStartPre}
+    ${execStart}
+  '';
 }
