2366adf2c3165df1e0fa12652de181108d40b604
Rhys <rimmington@gmail.com>
2016-05-13 10:44:15 +1000

    FIX: disenv: Copy entire manifest closure

--- pkgs/disquick/disenv.py
+++ pkgs/disquick/disenv.py
@@ -12,8 +12,64 @@ import tempfile
 sys.path.append('libexec')
 
 import argparse2man
+from cached_property import cached_property
 
 __description__ = 'Installs or updates the service environment of a remote system'
+
+class Remote():
+    def __init__(self, target, system, tempdir, ssh_user=None):
+        if ssh_user is None:
+            ssh_user = env.get('SSH_USER', env['USER'])
+        self.ssh_user = ssh_user
+        self.target = target
+        self.system = system
+        self.tempdir = tempdir
+
+    @cached_property
+    def env(self):
+        env = os.environ.copy()
+        env['TMPDIR'] = '/tmp'
+        env['DISNIX_IMPORT_SUDO'] = 'true'
+        env['SSH_USER'] = self.ssh_user
+        return env
+
+    @cached_property
+    def infrastructure(self):
+        return writefile(self.tempdir + "/infrastructure.nix", mk_infrastructure(self.target, self.system))
+
+    @cached_property
+    def local_profile_path(self):
+        d = os.path.expanduser('~/.local/share/disenv/') + self.target
+        os.makedirs(d, exist_ok=True, mode=0o700)
+        return d
+
+    @cached_property
+    def remote_profile_path(self):
+        return '{}@{}:/var/lib/disenv/coordinator-profile'.format(self.ssh_user, self.target)
+
+    def _run_disnix(self, cmd, **kwargs):
+        subprocess.check_call(['nix-shell', '-p', 'disnix', '--run', cmd], env=self.env, **kwargs)
+
+    def _rsync(self, here, there):
+        subprocess.check_call(['rsync', '-rl', here + '/', there])
+
+    def _sync_coordinator_profile(self, dir_flag):
+        for name in filter(lambda n: n != 'default', os.listdir(self.local_profile_path)):
+            nix_store_path = os.readlink(self.local_profile_path + '/' + name)
+            cmd = 'disnix-copy-closure {} -t {} {}'.format(dir_flag, self.target, nix_store_path)
+            self._run_disnix(cmd)
+
+    def pull_coordinator_profile(self):
+        self._rsync(self.remote_profile_path, self.local_profile_path)
+        self._sync_coordinator_profile('--from')
+
+    def push_coordinator_profile(self):
+        self._rsync(self.local_profile_path, self.remote_profile_path)
+        self._sync_coordinator_profile('--to')
+
+    def deploy(self, services, distribution):
+        cmd = 'disnix-env -i "{}" -d "{}" -s "{}" --show-trace --build-on-targets --coordinator-profile-path {}'.format(self.infrastructure, distribution, services, self.local_profile_path)
+        self._run_disnix(cmd)
 
 def mk_infrastructure(hostname, system):
     return """{{ target = {{ hostname = "{}"; system = "{}"; }}; }}""".format(hostname, system)
@@ -37,61 +93,23 @@ def get_service_names(filename, infrastructure):
     in_string = json.loads(out)
     return json.loads(in_string)
 
-def rsync_coordinator_profile(here, there):
-    subprocess.check_call(['rsync', '-rl', here + '/', there])
-
-def link_is_local(path):
-    dest = os.readlink(path)
-    d = os.path.dirname(dest)
-    return d == '.' or d == '' or d == '_actual'
-
-def copy_actual(path):
-    os.makedirs(path + '/_actual', exist_ok=True)  # TODO: should be mode of parent
-    for entry in os.scandir(path):
-        if entry.name != '_actual' and not (entry.is_symlink() and link_is_local(entry.path)):
-            actual = path + '/_actual/' + entry.name
-            shutil.copy2(entry.path, actual, follow_symlinks=True)
-            os.unlink(entry.path)
-            os.symlink('_actual/' + entry.name, entry.path)
-
-def get_remote_coordinator_profile(local, remote, user):
-    rsync_coordinator_profile('{}@{}:/var/lib/disenv/coordinator-profile'.format(user, remote), local)
-
-def update_remote_coordinator_profile(local, remote, user):
-    copy_actual(local)
-    rsync_coordinator_profile(local, '{}@{}:/var/lib/disenv/coordinator-profile'.format(user, remote))
-
-def local_profile_dir(target):
-    d = os.path.expanduser('~/.local/share/disenv/') + target
-    os.makedirs(d, exist_ok=True, mode=0o700)
-    return d
-
 def run(filename, target, system, tempdir, ssh_user=None):
     filename = os.path.abspath(filename)
 
-    infrastructure = writefile(tempdir + "/infrastructure.nix", mk_infrastructure(target, system))
-    service_names = get_service_names(filename, infrastructure)
-    distribution = writefile(tempdir + "/distribution.nix", mk_distribution(service_names))
-    services = writefile(tempdir + "/services.nix", mk_services(filename, infrastructure))
+    remote = Remote(target, system, tempdir, ssh_user=ssh_user)
 
-    env = os.environ.copy()
-    env['TMPDIR'] = '/tmp'
-    env['DISNIX_IMPORT_SUDO'] = 'true'
-    if ssh_user:
-        env['SSH_USER'] = ssh_user
-    else:
-        ssh_user = env.get('SSH_USER', env['USER'])
-    profile_path = local_profile_dir(target)
+    service_names = get_service_names(filename, remote.infrastructure)
+    distribution = writefile(tempdir + "/distribution.nix", mk_distribution(service_names))
+    services = writefile(tempdir + "/services.nix", mk_services(filename, remote.infrastructure))
 
     print('[disenv]: Retrieving coordinator profile from remote')
-    get_remote_coordinator_profile(profile_path, target, ssh_user)
+    remote.pull_coordinator_profile()
 
-    cmd = 'disnix-env -i "{}" -d "{}" -s "{}" --show-trace --build-on-targets --coordinator-profile-path {}'.format(infrastructure, distribution, services, profile_path)
     try:
-        subprocess.check_call(['nix-shell', '-p', 'disnix', '--run', cmd], env=env)
+        remote.deploy(services, distribution)
     finally:
         print('[disenv]: Sending coordinator profile to remote')
-        update_remote_coordinator_profile(profile_path, target, ssh_user)
+        remote.push_coordinator_profile()
 
 def main(argv):
     parser = argparse2man.new_parser(__description__)
@@ -108,4 +126,4 @@ def main(argv):
         run(args.services, args.target, args.system, tempdir, ssh_user=args.ssh_user)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
