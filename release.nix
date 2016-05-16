401896d082228875924fffdae65eaee79193824d
Rhys <rimmington@gmail.com>
2016-05-17 09:40:55 +1000

    QUALITY: Fix name in release.nix

--- release.nix
+++ release.nix
@@ -1,12 +1,12 @@
 { nixpkgs ? <nixpkgs>
 , systems ? ["x86_64-linux" "armv7l-linux"]
-, tanks-on-rails ? ./. }:
+, disquick ? ./. }:
 
 let
   lib = (import nixpkgs {}).lib;
 in lib.genAttrs systems (system:
   let
-    pkgs = import tanks-on-rails { pkgs = import nixpkgs { inherit system; }; };
+    pkgs = import disquick { pkgs = import nixpkgs { inherit system; }; };
   in {
     inherit (pkgs) disnixDebian disquick;
     # TODO: tests
