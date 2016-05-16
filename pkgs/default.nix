f9ac0f5e7df5820f343ebefce4a584e61f60cf31
Rhys <rimmington@gmail.com>
2016-05-16 15:03:03 +1000

    QUALITY: Move Dysnomia stuff to disnix dir

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
 pkgs.overridePackages (self: super: {
   mkService = self.callPackage ./mkService.nix {};
   disnix = self.callPackage ./disnix { inherit (super) disnix; };
-  dysnomiaDebian = self.callPackage ./dysnomia-debian.nix {};
+  dysnomiaDebian = self.callPackage ./disnix/dysnomiaDebian.nix {};
   disnixDebian = self.callPackage ./disnix/debian.nix {};
   disquick = self.callPackage ./disquick {};
   remoteShadow = self.callPackage ./remoteShadow.nix {};
