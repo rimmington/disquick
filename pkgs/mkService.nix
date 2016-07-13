f466d30780d4116fbdd5e245452cacf28b7ae028
Rhys <rimmington@gmail.com>
2016-07-13 10:30:06 +1000

    QUALITY: Minor mkService improvements

--- pkgs/mkService.nix
+++ pkgs/mkService.nix
@@ -29,34 +29,21 @@ assert killMode == "control-group" || killMode == "process";  # Strings are the 
 assert lib.all (p: if lib.isDerivation p then true else throw "Path must be constructed from derivations, but found a ${builtins.typeOf p} in the path of ${name}") path;
 
 let
-  chpst = lib.overrideDerivation runit (o: {
-    name = "runit-${o.version}-chpst";
-    # https://github.com/NixOS/nixpkgs/blob/7a37ac15b3fdca4dfa5c16fcc2a4de1294f5dc87/pkgs/tools/system/runit/default.nix
-    phases = [ "unpackPhase" "patchPhase" "buildPhase" "checkPhase" "installPhase" "fixupPhase" ];
-    postPatch = ''
-      cd ${o.name}
-      sed -i 's,-static,,g' src/Makefile
-    '';
-    buildPhase = ''
-      make -C 'src'
-    '';
-    installPhase = "mv src/chpst $out";
-  });
   user =
     let a = { name = "root"; groups = []; home = null; allowLogin = false; } // (attrs.user or {});
     in { create = a.name != "root"; createHome = a.home != null; } // a;
-  service =
-    let depNames = map (s: s.attrs.name + ".service") dependsOn;
-    in {
-      inherit description environment path;
-      wantedBy = if startWithBoot then [ "multi-user.target" ] else [];
-      serviceConfig = { ExecStart = execStart; }
-        // commonServiceAttrs
-        // lib.optionalAttrs (execStartPre != "") { ExecStartPre = execStartPre; }
-        // lib.optionalAttrs (execStartPost != "") { ExecStartPost = execStartPost; };
-      wants = depNames;
-      after = depNames;
-    };
+  commonServiceAttrs = {
+    KillMode = killMode;
+    Restart =
+      if restartOnSuccess && restartOnFailure
+        then "always"
+      else if restartOnFailure
+        then "on-failure"
+      else if restartOnSuccess
+        then "on-success"
+      else   "no";
+  } // lib.optionalAttrs (execStartPre != "") { ExecStartPre = execStartPre; }
+    // lib.optionalAttrs (execStartPost != "") { ExecStartPost = execStartPost; };
   execStartPre = if preStartRootScript != "" || user.create
     then writeScript "${name}-prestart" ''
       #!${stdenv.shell} -e
@@ -81,12 +68,6 @@ let
       ''}
       ${preStartRootScript}''
     else "";
-  runAsUser = exec:
-    if user.name == "root"
-      then exec
-      else
-        let suf = lib.optionalString (user.groups != []) ":${lib.concatStringsSep ":" user.groups}";
-        in "${chpst} -u ${user.name}${suf} ${exec}";
   execStart =
     let
       userScript = writeScript "${name}-start" ''
@@ -95,51 +76,71 @@ let
         ${script}'';
     in runAsUser "${userScript}";
   execStartPost = lib.optionalString (postStartScript != "") (runAsUser "${writeScript "${name}-poststart" "#!${stdenv.shell} -e\n${postStartScript}"}");
-  commonServiceAttrs = {
-    KillMode = killMode;
-    Restart =
-      if restartOnSuccess && restartOnFailure
-        then "always"
-      else if restartOnFailure
-        then "on-failure"
-      else if restartOnSuccess
-        then "on-success"
-      else   "no";
-  };
+  runAsUser = exec:
+    if user.name == "root"
+      then exec
+      else
+        let suf = lib.optionalString (user.groups != []) ":${lib.concatStringsSep ":" user.groups}";
+        in "${chpst} -u ${user.name}${suf} ${exec}";
   pathValue =
     let
       defaultPathPkgs = [ coreutils findutils gnugrep gnused systemd ];
       finalPath = path ++ defaultPathPkgs;
     in lib.concatStringsSep ":" (map (d: "${d}/bin") finalPath ++ map (d: "${d}/sbin") finalPath);
   envDeclsGen = prefix: (lib.mapAttrsToList (name: value: "${prefix}${name}=${value}") (environment // { PATH = pathValue; }));
+  chpst = lib.overrideDerivation runit (o: {
+    name = "runit-${o.version}-chpst";
+    # https://github.com/NixOS/nixpkgs/blob/7a37ac15b3fdca4dfa5c16fcc2a4de1294f5dc87/pkgs/tools/system/runit/default.nix
+    phases = [ "unpackPhase" "patchPhase" "buildPhase" "checkPhase" "installPhase" "fixupPhase" ];
+    postPatch = ''
+      cd ${o.name}
+      sed -i 's,-static,,g' src/Makefile
+    '';
+    buildPhase = ''
+      make -C 'src'
+    '';
+    installPhase = "mv src/chpst $out";
+  });
 in {
   inherit attrs;
 
-  serviceAttr = builtins.listToAttrs [ { inherit name; value = service; } ];
+  serviceAttr = {
+    "${name}" =
+      let depNames = map (s: s.attrs.name + ".service") dependsOn;
+      in {
+        inherit description environment path;
+        wantedBy = if startWithBoot then [ "multi-user.target" ] else [];
+        serviceConfig = { ExecStart = execStart; } // commonServiceAttrs;
+        wants = depNames;
+        after = depNames;
+      };
+  };
 
   disnix =
-    # TODO: Avoid making three separate derivations here
-    let _pkg = buildEnv {
+    let _pkg = stdenv.mkDerivation {
       name = "service-${name}";
-      paths = [
-        (writeTextFile { name = "${name}-disnix-process-config"; destination = "/etc/process_config"; text = ''container_process="${execStart}"''; })
-        (writeTextFile { name = "${name}-disnix-systemd-config"; destination = "/etc/systemd-config"; text =
-          let
-            section = name: items: lib.optionals (items != []) ([ "[${name}]" ] ++ items ++ [ "" ]);
-            unit = section "Unit" (
-              lib.optional (description != "") "Description=${description}" ++
-              lib.optionals (dependsOn != []) (
-                let value = lib.concatMapStringsSep " " (s: "disnix-${baseNameOf s.disnix._pkg.outPath}.service") dependsOn;
-                in [ "Wants=${value}" "After=${value}" ]));
-            install = section "Install" (
-              lib.optional startWithBoot "WantedBy=multi-user.target");
-            service = section "Service" (
-              (envDeclsGen "Environment=") ++
-              (lib.optional (execStartPre != "") "ExecStartPre=${execStartPre}") ++
-              (lib.optional (execStartPost != "") "ExecStartPost=${execStartPost}") ++
-              (lib.mapAttrsToList (name: value: "${name}=${value}") commonServiceAttrs));
-          in "\n" + lib.concatStringsSep "\n" (unit ++ install ++ service); })
-      ];
+      passAsFile = [ "processConfig" "systemdConfig" ];
+      buildCommand = ''
+        mkdir -p $out/etc
+        cp $processConfigPath $out/etc/process_config
+        cp $systemdConfigPath $out/etc/systemd-config
+      '';
+
+      processConfig = ''container_process="${execStart}"'';
+      systemdConfig =
+        let
+          section = name: items: lib.optionals (items != []) ([ "[${name}]" ] ++ items ++ [ "" ]);
+          unit = section "Unit" (
+            lib.optional (description != "") "Description=${description}" ++
+            lib.optionals (dependsOn != []) (
+              let value = lib.concatMapStringsSep " " (s: "disnix-${baseNameOf s.disnix._pkg.outPath}.service") dependsOn;
+              in [ "Wants=${value}" "After=${value}" ]));
+          install = section "Install" (
+            lib.optional startWithBoot "WantedBy=multi-user.target");
+          service = section "Service" (
+            (envDeclsGen "Environment=") ++
+            (lib.mapAttrsToList (name: value: "${name}=${value}") commonServiceAttrs));
+        in "\n" + lib.concatStringsSep "\n" (unit ++ install ++ service);
     };
     in {
       inherit name _pkg;
