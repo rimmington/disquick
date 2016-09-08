aa00ff1d70b1b104f19817311201d08cb47d6150
Rhys <rimmington@gmail.com>
2016-09-08 10:52:48 +1000

    FIX: disnix: Prevent "argument list too long" errors from disnix-copy-closure

--- pkgs/disnix/default.nix
+++ pkgs/disnix/default.nix
@@ -1,6 +1,6 @@
 {lib, disnix, help2man, ...}:
 
 lib.overrideDerivation disnix (o: {
-  patches = [ ./armv7l.patch ./disnix-import-sudo.patch ];
+  patches = [ ./armv7l.patch ./disnix-import-sudo.patch ./argument-list-too-long.patch ];
   buildInputs = o.buildInputs ++ [ help2man ];
 })
