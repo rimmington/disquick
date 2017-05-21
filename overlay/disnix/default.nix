d98e1a038e35f558d8bd4f07709b064dc471aa92
Rhys <rimmington@gmail.com>
2017-05-22 08:47:54 +1000

    FIX: Patch Disnix to build max 2 outputs at a time, mitigates Pi lock-up

--- overlay/disnix/default.nix
+++ overlay/disnix/default.nix
@@ -1,6 +1,6 @@
 {lib, disnix, help2man, ...}:
 
 lib.overrideDerivation disnix (o: {
-  patches = [ ./disnix-import-sudo.patch ./argument-list-too-long.patch ./send-outputs.patch ];
+  patches = [ ./disnix-import-sudo.patch ./argument-list-too-long.patch ./send-outputs.patch ./build-max-2.patch ];
   buildInputs = o.buildInputs ++ [ help2man ];
 })
